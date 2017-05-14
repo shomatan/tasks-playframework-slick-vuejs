4885c01501309d11b071fcc4ff96a1adf1fe2b95
Shoma Nishitateno <shoma416@gmail.com>
2017-05-14 13:19:25 +0900

    Get tasks.

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,48 @@
-axios.get('/api/v1/tasks')
-    .then(function (response) {
-        initVue(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
 
-function initVue(tasks){
-    new Vue({
-        el: '#app',
-        data: {
-            tasks: tasks
-        },
-        mounted: function(){
-            document.getElementById("app").style.display = "";
+var TaskList = {
+    template: '#task-list',
+    data: function () {
+        return {
+            isLoading: false,
+            tasks: [],
+            error: null
         }
-    })
+    },
+
+    created: function () {
+        this.fetchData()
+    },
+
+    watch: {
+        '$route': 'fetchData'
+    },
+
+    methods: {
+        fetchData: function () {
+            var self = this
+            self.isLoading = true
+            axios.get('/api/v1/tasks')
+                .then(function (response) {
+                    self.tasks = response.data
+                    self.isLoading = false
+                })
+                .catch(function (error) {
+                    self.fetchError = error
+                    self.isLoading = false
+                })
+        }
+    }
 }
+
+var router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: TaskList
+        }
+    ]
+})
+
+new Vue({
+    router: router
+}).$mount('#app')
