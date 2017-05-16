be1e92d1f98912c3993171f39e3a302188efcf5e
Shoma Nishitateno <shoma416@gmail.com>
2017-05-16 09:17:51 +0900

    load bootstrap.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,11 @@ module.exports = {
         test: /\.png$/,
         loader: "url-loader?mimetype=image/png"
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
     ]
   },
   resolve: {
