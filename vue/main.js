ea710dfbdcdf0b8750a99ba95c610ffdb6e15231
Shoma Nishitateno <shoma416@gmail.com>
2017-05-22 17:10:46 +0900

    Add element-ui

--- vue/main.js
+++ vue/main.js
@@ -2,6 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'bootstrap/dist/css/bootstrap.css';
 import Vue from 'vue'
+
+// element-ui
+import { DatePicker } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+// app
 import App from './App.vue'
 import Home from './components/Home.vue'
 import Add from './components/task/Add.vue'
@@ -12,6 +19,8 @@ import VueRouter from 'vue-router'
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(DatePicker)
+locale.use(lang)
 
 // Pointing routes to the components they should use
 var router = new VueRouter({
--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,18 @@ module.exports = {
         loader: 'vue-loader'
       },
       {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+      },
+      {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+      },
+      {
         test: /\.png$/,
         loader: "url-loader?mimetype=image/png"
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
       { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
